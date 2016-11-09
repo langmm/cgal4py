6ab5ff665cb80ff728a1c961657fae552ef379eb
Meagan Lang <langmm.astro@gmail.com>
2016-11-09 21:14:06 +0000

    Partial flake8 fixes

--- cgal4py/tests/test_delaunay2.py
+++ cgal4py/tests/test_delaunay2.py
@@ -1,23 +1,26 @@
-# TODO:
-# - value test circumcenter
-# - test includes_edge
+r"""Tests for 2D Delaunay triangulation.
 
-from nose import with_setup
+.. todo::
+   * value test circumcenter
+   * test includes_edge
+   * answer testing for voronoi volumes
+"""
 import numpy as np
 import os
 from cgal4py.delaunay import Delaunay2
 
+
 left_edge = -2*np.ones(2, 'float64')
 right_edge = 2*np.ones(2, 'float64')
-pts = np.array([[-0.4941988586954018 , -0.07594397977563715],
-                [-0.06448037284989526,  0.4958248496365813 ],
-                [ 0.4911154367094632 ,  0.09383830681375946],
-                [-0.348353580869097  , -0.3586778257652367 ],
+pts = np.array([[-0.49419885869540180, -0.07594397977563715],
+                [-0.06448037284989526,  0.49582484963658130],
+                [+0.49111543670946320,  0.09383830681375946],
+                [-0.34835358086909700, -0.35867782576523670],
                 [-1,     -1],
                 [-1,      1],
-                [ 1,     -1],
-                [ 1,      1]], 'float64')
-pts_dup = np.concatenate([pts, np.reshape(pts[0,:],(1,pts.shape[1]))])
+                [+1,     -1],
+                [+1,      1]], 'float64')
+pts_dup = np.concatenate([pts, np.reshape(pts[0, :], (1, pts.shape[1]))])
 nverts_fin = pts.shape[0]
 nverts_inf = 1
 nverts = nverts_fin + nverts_inf
@@ -28,8 +31,11 @@ ncells_fin = 10
 ncells_inf = 4
 ncells = ncells_fin + ncells_inf
 
+
 def test_create():
     T = Delaunay2()
+    del(T)
+
 
 def test_insert():
     # without duplicates
@@ -41,12 +47,14 @@ def test_insert():
     T.insert(pts_dup)
     assert(T.is_valid())
 
+
 def test_equal():
     T1 = Delaunay2()
     T1.insert(pts)
     T2 = Delaunay2()
     T2.insert(pts)
     assert(T1.is_equivalent(T2))
+
 
 def test_num_verts():
     # without duplicates
@@ -64,6 +72,7 @@ def test_num_verts():
     assert(T.num_infinite_verts == nverts_inf)
     assert(T.num_verts == nverts)
 
+
 def test_num_edges():
     # without duplicates
     T = Delaunay2()
@@ -79,6 +88,7 @@ def test_num_edges():
     assert(T.num_finite_edges == nedges_fin)
     assert(T.num_infinite_edges == nedges_inf)
     assert(T.num_edges == nedges)
+
 
 def test_num_cells():
     # without duplicates
@@ -96,6 +106,7 @@ def test_num_cells():
     assert(T.num_infinite_cells == ncells_inf)
     assert(T.num_cells == ncells)
 
+
 def test_all_verts():
     T = Delaunay2()
     T.insert(pts)
@@ -111,6 +122,7 @@ def test_all_verts():
     assert(count_inf == T.num_infinite_verts)
     assert(count == T.num_verts)
 
+
 def test_finite_verts():
     T = Delaunay2()
     T.insert(pts)
@@ -119,6 +131,7 @@ def test_finite_verts():
         assert((not v.is_infinite()))
         count += 1
     assert(count == T.num_finite_verts)
+
 
 def test_all_edges():
     T = Delaunay2()
@@ -135,6 +148,7 @@ def test_all_edges():
     assert(count_inf == T.num_infinite_edges)
     assert(count == T.num_edges)
 
+
 def test_finite_edges():
     T = Delaunay2()
     T.insert(pts)
@@ -144,6 +158,7 @@ def test_finite_edges():
         count += 1
     print(count)
     assert(count == T.num_finite_edges)
+
 
 def test_all_cells():
     T = Delaunay2()
@@ -159,6 +174,7 @@ def test_all_cells():
     assert(count_inf == T.num_infinite_cells)
     assert(count == T.num_cells)
 
+
 def test_finite_cells():
     T = Delaunay2()
     T.insert(pts)
@@ -168,6 +184,7 @@ def test_finite_cells():
         count += 1
     assert(count == T.num_finite_cells)
 
+
 def test_clear():
     T = Delaunay2()
     T.insert(pts)
@@ -175,6 +192,7 @@ def test_clear():
     print(T.num_finite_verts, T.num_cells)
     assert(T.num_finite_verts == 0)
     assert(T.num_cells == 0)
+
 
 def test_io():
     fname = 'test_io2348_2.dat'
@@ -187,22 +205,24 @@ def test_io():
     assert(Tout.num_cells == Tin.num_cells)
     os.remove(fname)
 
+
 def test_serialize():
     Tout = Delaunay2()
     Tout.insert(pts)
-    f,n,i = Tout.serialize()
+    f, n, i = Tout.serialize()
     Tin = Delaunay2()
-    Tin.deserialize(pts,f,n,i)
+    Tin.deserialize(pts, f, n, i)
     assert(Tout.num_verts == Tin.num_verts)
     assert(Tout.num_cells == Tin.num_cells)
+
 
 def test_locate():
     T = Delaunay2()
     T.insert(pts)
     for c in T.finite_cells:
         p = c.center
-        print('{}\n{}\n{}'.format(c,p,T.locate(p)))
-        assert(c == T.locate(p,c))
+        print('{}\n{}\n{}'.format(c, p, T.locate(p)))
+        assert(c == T.locate(p, c))
         assert(c == T.locate(p))
         assert(c.vertex(0) == T.locate(c.vertex(0).point))
         assert(c.vertex(0) == T.locate(c.vertex(0).point, c))
@@ -210,12 +230,14 @@ def test_locate():
         # assert(c.edge(0) == T.locate(c.edge(0).midpoint, c))
         break
 
+
 def test_get_vertex():
     T = Delaunay2()
     T.insert(pts)
     for i in range(nverts_fin):
         v = T.get_vertex(i)
-        assert(np.allclose(v.point, pts[i,:]))
+        assert(np.allclose(v.point, pts[i, :]))
+
 
 def test_remove():
     T = Delaunay2()
@@ -224,17 +246,21 @@ def test_remove():
     T.remove(v)
     assert(T.num_verts == (nverts-1))
 
+
 def test_is_edge():
     T = Delaunay2()
     T.insert(pts)
     assert(T.is_edge(T.get_vertex(0), T.get_vertex(1)))
     assert(not T.is_edge(T.get_vertex(0), T.get_vertex(nverts_fin-1)))
 
+
 def test_is_cell():
     T = Delaunay2()
     T.insert(pts)
     assert(T.is_cell(T.get_vertex(0), T.get_vertex(1), T.get_vertex(3)))
-    assert(not T.is_cell(T.get_vertex(0), T.get_vertex(1), T.get_vertex(nverts_fin-1)))
+    assert(not T.is_cell(T.get_vertex(0), T.get_vertex(1),
+                         T.get_vertex(nverts_fin-1)))
+
 
 def test_vert():
     T = Delaunay2()
@@ -253,13 +279,14 @@ def test_vert():
             assert(idx == np.iinfo(np.uint32).max)
             assert(np.isclose(vol, -1))
         else:
-            assert(np.allclose(pnt, pts[idx,:]))
+            assert(np.allclose(pnt, pts[idx, :]))
             if idx >= 4:
                 assert(np.isclose(vol, -1))
             c = v.cell
             v.set_cell(c)
             v.set_point(pnt)
         vold = v
+
 
 def test_edge():
     T = Delaunay2()
@@ -279,9 +306,10 @@ def test_edge():
             assert(np.isclose(elen, -1.0))
             assert(np.isinf(e.center).all())
         else:
-            l = np.sqrt(np.sum((pts[v1.index,:]-pts[v2.index,:])**2.0))
+            l = np.sqrt(np.sum((pts[v1.index, :]-pts[v2.index, :])**2.0))
             assert(np.isclose(elen, l))
         eold = e
+
 
 def test_cell():
     T = Delaunay2()
@@ -299,8 +327,9 @@ def test_cell():
         e2 = c.edge(1)
         e3 = c.edge(2)
         assert(c.has_vertex(v1))
-        assert(c.has_vertex(v1, return_index = True) == 0)
+        assert(c.has_vertex(v1, return_index=True) == 0)
         assert(c.ind_vertex(v1) == 0)
+        del(e1, e2, e3)
 
         c.reset_vertices()
         c.set_vertex(0, v1)
@@ -310,7 +339,7 @@ def test_cell():
         n2 = c.neighbor(1)
         n3 = c.neighbor(2)
         assert(c.has_neighbor(n1))
-        assert(c.has_neighbor(n1, return_index = True) == 0)
+        assert(c.has_neighbor(n1, return_index=True) == 0)
         assert(c.ind_neighbor(n1) == 0)
 
         c.reset_neighbors()
@@ -322,7 +351,8 @@ def test_cell():
         print(c.side(2*v1.point-c.center))
         print(c.side_of_circle(c.circumcenter))
         print(c.side_of_circle(v1.point))
-        print(c.side_of_circle(2*v1.point-c.circumcenter), 2*v1.point-c.circumcenter)
+        print(c.side_of_circle(2*v1.point-c.circumcenter),
+              2*v1.point-c.circumcenter)
         if c.is_infinite():
             assert(np.isinf(c.center).all())
             assert(c.side(c.center) == -1)
@@ -351,7 +381,7 @@ def test_move():
     T = Delaunay2()
     T.insert(pts)
     v0 = T.get_vertex(0)
-    new_pos = np.zeros(2,'float64')
+    new_pos = np.zeros(2, 'float64')
     v = T.move(v0, new_pos)
     assert(np.allclose(v.point, new_pos))
     assert(np.allclose(v0.point, new_pos))
@@ -360,32 +390,35 @@ def test_move():
     assert(np.allclose(v.point, new_pos))
     assert(T.num_verts == (nverts-1))
 
+
 def test_move_if_no_collision():
     T = Delaunay2()
     T.insert(pts)
     v0 = T.get_vertex(0)
-    new_pos = np.zeros(2,'float64')
+    new_pos = np.zeros(2, 'float64')
     v = T.move_if_no_collision(v0, new_pos)
     assert(np.allclose(v.point, new_pos))
     assert(np.allclose(v0.point, new_pos))
     v1 = T.get_vertex(1)
     v = T.move_if_no_collision(v1, new_pos)
     assert(np.allclose(v.point, new_pos))
-    assert(np.allclose(v1.point, pts[1,:]))
+    assert(np.allclose(v1.point, pts[1, :]))
     assert(T.num_verts == nverts)
+
 
 def test_flip():
     T = Delaunay2()
     T.insert(pts)
     for c in T.all_cells:
         out = T.flip(c, 0)
-        assert(out == True)
+        assert(out)
     print(T.num_edges, nedges)
     assert(T.num_edges == nedges)
     for e in T.all_edges:
         out = e.flip()
-        assert(out == True)
+        assert(out)
     print(T.num_edges, nedges)
+
 
 def test_flippable():
     T = Delaunay2()
@@ -397,6 +430,7 @@ def test_flippable():
     for e in T.all_edges:
         e.flip()
     print(T.num_edges, nedges)
+
 
 def test_vert_incident_verts():
     T = Delaunay2()
@@ -411,6 +445,7 @@ def test_vert_incident_verts():
     print(count)
     assert(count == 42)
 
+
 def test_vert_incident_edges():
     T = Delaunay2()
     T.insert(pts)
@@ -423,6 +458,7 @@ def test_vert_incident_edges():
         print(v.index, c0)
     print(count)
     assert(count == 42)
+
 
 def test_vert_incident_cells():
     T = Delaunay2()
@@ -437,6 +473,7 @@ def test_vert_incident_cells():
     print(count)
     assert(count == 42)
 
+
 def test_edge_incident_verts():
     T = Delaunay2()
     T.insert(pts)
@@ -449,6 +486,7 @@ def test_edge_incident_verts():
         print(c0)
     print(count)
     assert(count == 42)
+
 
 def test_edge_incident_edges():
     T = Delaunay2()
@@ -463,6 +501,7 @@ def test_edge_incident_edges():
     print(count)
     assert(count == 156)
 
+
 def test_edge_incident_cells():
     T = Delaunay2()
     T.insert(pts)
@@ -475,6 +514,7 @@ def test_edge_incident_cells():
         print(c0)
     print(count)
     assert(count == 100)
+
 
 def test_cell_incident_verts():
     T = Delaunay2()
@@ -489,6 +529,7 @@ def test_cell_incident_verts():
     print(count)
     assert(count == 42)
 
+
 def test_cell_incident_edges():
     T = Delaunay2()
     T.insert(pts)
@@ -501,6 +542,7 @@ def test_cell_incident_edges():
         print(c0)
     print(count)
     assert(count == 42)
+
 
 def test_cell_incident_cells():
     T = Delaunay2()
@@ -515,18 +557,21 @@ def test_cell_incident_cells():
     print(count)
     assert(count == 42)
 
+
 def test_nearest_vertex():
     idx_test = 7
     T = Delaunay2()
     T.insert(pts)
-    v = T.nearest_vertex(pts[idx_test,:]-0.1)
+    v = T.nearest_vertex(pts[idx_test, :]-0.1)
     assert(v.index == idx_test)
+
 
 def test_mirror():
     T = Delaunay2()
     T.insert(pts)
     for e in T.all_edges:
         e2 = T.mirror_edge(e)
+        del(e2)
         break
     for c in T.all_cells:
         idx = 0
@@ -534,6 +579,7 @@ def test_mirror():
         assert(c == c.neighbor(idx).neighbor(i2))
         v2 = T.mirror_vertex(c, idx)
         assert(v2 == c.neighbor(idx).vertex(i2))
+
 
 def test_get_boundary_of_conflicts():
     T = Delaunay2()
@@ -544,6 +590,7 @@ def test_get_boundary_of_conflicts():
     edges = T.get_boundary_of_conflicts(p, c)
     print(len(edges))
 
+
 def test_get_conflicts():
     T = Delaunay2()
     T.insert(pts)
@@ -552,6 +599,7 @@ def test_get_conflicts():
     p = c.circumcenter
     cells = T.get_conflicts(p, c)
     print(len(cells))
+
 
 def test_get_conflicts_and_boundary():
     T = Delaunay2()
@@ -562,6 +610,7 @@ def test_get_conflicts_and_boundary():
     cells, edges = T.get_conflicts_and_boundary(p, c)
     print(len(cells), len(edges))
 
+
 def test_line_walk():
     T = Delaunay2()
     T.insert(pts)
@@ -571,6 +620,7 @@ def test_line_walk():
     print(len(x))
     assert(len(x) == 6)
 
+
 def test_vertices():
     T = Delaunay2()
     T.insert(pts)
@@ -579,6 +629,7 @@ def test_vertices():
     assert(v.shape[1] == pts.shape[1])
     assert(np.allclose(pts, v))
 
+
 def test_edges():
     T = Delaunay2()
     T.insert(pts)
@@ -586,28 +637,31 @@ def test_edges():
     assert(e.shape[0] == T.num_finite_edges)
     assert(e.shape[1] == 2)
 
+
 def test_boundary_points():
     T = Delaunay2()
     T.insert(pts)
     out = T.boundary_points(left_edge, right_edge, False)
+    del(out)
+
 
 def test_outgoing_points():
     T = Delaunay2()
     T.insert(pts)
-    le = np.array([[-2,-2],
-                   [-1,-1],
-                   [ 0, 0],
-                   [ 1, 1]], 'float64')
-    re = np.array([[-1,-1],
-                   [ 0, 0],
-                   [ 1, 1],
-                   [ 2, 2]], 'float64')
+    le = np.array([[-2, -2],
+                   [-1, -1],
+                   [+0,  0],
+                   [+1,  1]], 'float64')
+    re = np.array([[-1, -1],
+                   [+0,  0],
+                   [+1,  1],
+                   [+2,  2]], 'float64')
     out = T.outgoing_points(le, re)
     assert(len(out) == le.shape[0])
+
 
 def test_voronoi_volumes():
     T = Delaunay2()
     T.insert(pts)
     v = T.voronoi_volumes()
     assert(v.shape[0] == T.num_finite_verts)
-    # TODO: answer testing
