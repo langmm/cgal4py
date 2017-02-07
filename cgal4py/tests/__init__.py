171301040830070949b112e46e7d82a25ac8aa19
Meagan Lang <langmm.astro@gmail.com>
2017-02-07 03:54:10 +0000

    Remove test_utils from testing package

--- cgal4py/tests/__init__.py
+++ cgal4py/tests/__init__.py
@@ -2,9 +2,8 @@ import test_cgal4py
 import test_delaunay2
 import test_delaunay3
 import test_plot
-import test_utils
 import scaling
 
 
 __all__ = ["test_cgal4py", "test_delaunay2", "test_delaunay3", "test_plot",
-           "test_utils", "scaling"]
+           "scaling"]
